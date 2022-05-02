f01c3a7af003efd487aaa82cc68b570e9ac4c39d
Rodriguez Lautaro <lautaro.rodriguez553@gmail.com>
2022-05-02 16:01:15 -0300

    }Agregando y editando templates y peticiones de archivos segun el filtro

--- sheet/urls.py
+++ sheet/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name="hojas"
 urlpatterns = [
     path('',views.index, name="index"),
-    path('services_lists/',views.ServiceView.as_view(), name="services_lists")
+    path('services_lists/',views.ServiceView.as_view(), name="services_lists"),
+    path('bills_lists/', views.BillView.as_view(), name ="bills_lists"),
+    path("add_service/", views.AddService.as_view(), name="add_service")
 ]
--- sheet/views.py
+++ sheet/views.py
@@ -1,6 +1,3 @@
-from http.client import HTTPResponse
-from msilib.schema import ServiceInstall
-from django.http import Http404, HttpResponse,HttpRequest,QueryDict
 from django.shortcuts import render
 from django.views import View, generic
 from .models import Service
@@ -18,16 +15,86 @@ class ServiceView(generic.ListView):
     # queryset = Service.objects.all()
 
     def get_queryset(self) :
-        nashe=self.request.GET.get("time")
-        if nashe == "esta_semana":
+        response_time=self.request.GET.get("time")
+        response_pick_folder = self.request.GET.get("pick_folder")
+
+        today = date.today()
+        day = today.day
+        month = today.month
+        year= today.year
+
+        if response_time == "last_week" and response_pick_folder == "sin_filtros":
             start = date.today()
-            end = date.today() - datetime.timedelta(days=6)
-            print(start)
-            print(end)
-            print(Service.date)
-            return Service.objects.filter(date__gte =end)
+            end = date.today() - datetime.timedelta(days=7)
+            return Service.objects.filter(date__range=[end,start])
+        elif response_time == "this_month" and response_pick_folder == "sin_filtros":
+            start = date(year,month,1)
+            end = date(year,month,day)
+            return Service.objects.filter(date__range =[start,end])
+        elif response_time =="sin_filtros" and response_pick_folder == "sin_filtros":
+            return Service.objects.all()
+
+
+        elif response_pick_folder =="destiempo" and  response_time == "sin_filtros":
+            return Service.objects.filter(date__range =[date(year,month,21), date(year,month,30)])
+        elif response_pick_folder =="aTiempo" and  response_time == "sin_filtros":
+            return Service.objects.filter(date__range =[date(year,month,1), date(year,month,21)])
+
+
+        elif response_pick_folder =="destiempo" and  response_time == "last_week":
+            return Service.objects.filter(date__range =[date(year,month,21), date(year,month,31)])
+        
         else:
             return Service.objects.all()
+        
+
+class BillView(generic.ListView):
+    template_name = "sheet/bills.html"
+    context_object_name = "list_bills"
+    # queryset = Service.objects.all()
+
+    def get_queryset(self) :
+        response_time=self.request.GET.get("time")
+        response_pick_folder = self.request.GET.get("pick_folder")
+
+        today = date.today()
+        day = today.day
+        month = today.month
+        year= today.year
+
+        if response_time == "last_week" and response_pick_folder == "sin_filtros":
+            start = date.today()
+            end = date.today() - datetime.timedelta(days=7)
+            return Service.objects.filter(date__range=[end,start])
+        elif response_time == "this_month" and response_pick_folder == "sin_filtros":
+            start = date(year,month,1)
+            end = date(year,month,day)
+            return Service.objects.filter(date__range =[start,end])
+        elif response_time =="sin_filtros" and response_pick_folder == "sin_filtros":
+            return Service.objects.all()
+
+
+        elif response_pick_folder =="destiempo" and  response_time == "sin_filtros":
+            return Service.objects.filter(date__range =[date(year,month,21), date(year,month,30)])
+        elif response_pick_folder =="aTiempo" and  response_time == "sin_filtros":
+            return Service.objects.filter(date__range =[date(year,month,1), date(year,month,21)])
+
+
+        elif response_pick_folder =="destiempo" and  response_time == "last_week":
+            return Service.objects.filter(date__range =[date(year,month,21), date(year,month,31)])
+        
+        else:
+            return Service.objects.all()
+        
+
+class AddService(generic.ListView):
+    template_name = "sheet/add_service.html"
+    context_object_name = "asd"
+
+    def get_queryset(self):
+        return Service.objects.all()
+     
+        
 
 
     
